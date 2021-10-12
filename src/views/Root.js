b210c22f370c9c8ad4021fb3dc9403407040d90e
maras20193 <lucas20193@gmail.com>
2021-10-12 13:12:44 +0200

    add UsersContext

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import UsersList from 'components/organisms/UsersList/UsersList';
 import { ThemeProvider } from 'styled-components';
 import MainTamplate from 'components/templates/MainTamplate/MainTamplate';
 import { Wrapper } from './Root.styles';
@@ -8,6 +7,8 @@ import Dashboard from './Dashboard';
 
 import { GlobalStyle } from 'assets/styles/globalStyles';
 import { theme } from 'assets/styles/theme';
+import UsersProvider from 'providers/UsersProvider';
+import AddUser from './AddUser';
 
 function Root() {
   return (
@@ -15,16 +16,18 @@ function Root() {
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <MainTamplate>
-          <Wrapper>
-            <Switch>
-              <Route>
-                <UsersList exact path="/" />
-              </Route>
-              <Route>
-                <Dashboard path="/add-user" />
-              </Route>
-            </Switch>
-          </Wrapper>
+          <UsersProvider>
+            <Wrapper>
+              <Switch>
+                <Route exact path="/">
+                  <Dashboard />
+                </Route>
+                <Route path="/add-user">
+                  <AddUser />
+                </Route>
+              </Switch>
+            </Wrapper>
+          </UsersProvider>
         </MainTamplate>
       </ThemeProvider>
     </Router>
